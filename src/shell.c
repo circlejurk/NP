22fa29e7330c7264aa5a38fdfa8e3688bca5dc7e
niorehkids <forendef2846@gmail.com>
2016-10-23 03:30:39 +0800

    numbered pipe finished

--- src/shell.c
+++ src/shell.c
@@ -16,6 +16,7 @@
 
 #define MAX_LINE_SIZE	15000
 #define MAX_CMD_SIZE	256
+#define MAX_PIPE	1000
 
 
 const char motd[] = "****************************************\n"
@@ -24,13 +25,18 @@ const char motd[] = "****************************************\n"
 const char prompt[] = "% ";
 
 void save_stdfds (int *stdfd);
+void restore_stdfds (int *stdfd);
 int readline (char *line, int *connection);
-int check_illegal (const char *line);
+int check_illegal (char *line);
+int arespace (char *s);
+
+void set_npipe (char *line, int pipefd[MAX_PIPE + 1][2], int *pndx, int *pipec, int *connection);
+int isnumber (char *s);
 
 int line_to_cmds (char *line, char **cmds);
 void clear_cmds (int progc, char **cmds);
 
-void execute_one_line (int progc, char **cmds, int *stdfd, int *connection);
+void execute_one_line (int progc, char **cmds, int *connection);
 void create_pipe (int *pipefd);
 int cmd_to_argv (char *cmd, char **argv, char **in_file, char **out_file);
 void clear_argv (int argc, char **argv, char **in_file, char **out_file);
@@ -44,15 +50,13 @@ void set_pipes_in (int *pipefd, int *stdfd, int index, int progc);
 
 void shell (void)
 {
-	int	connection = 1, progc, stdfd[2];
+	int	connection = 1, progc, stdfd[3];
+	int	pipefd[MAX_PIPE + 1][2], pndx[MAX_PIPE] = {0}, pipec = 1;
 	char	line[MAX_LINE_SIZE + 1], *cmds[(MAX_LINE_SIZE - 1) / 4];
 
 	/* initialize the environment variables */
 	clearenv ();
 	putenv ("PATH=bin:.");
-
-	/* save original stdin, stdout */
-	save_stdfds (stdfd);
 
 	/* print the welcome message */
 	write (STDOUT_FILENO, motd, sizeof(motd));
@@ -63,12 +67,20 @@ void shell (void)
 
 		/* read one line from client input */
 		if (readline (line, &connection) == 0) {
+			/* save original fds */
+			save_stdfds (stdfd);
+
+			/* set up the numbered pipe if exists */
+			set_npipe (line, pipefd, pndx, &pipec, &connection);
 
 			/* parse the total input line into commands seperated by pipes */
 			progc = line_to_cmds (line, cmds);
 
 			/* execute one line command */
-			execute_one_line (progc, cmds, stdfd, &connection);
+			execute_one_line (progc, cmds, &connection);
+
+			/* restore original fds */
+			restore_stdfds (stdfd);
 
 			/* free the allocated space of commands */
 			clear_cmds (progc, cmds);
@@ -76,11 +88,63 @@ void shell (void)
 	}
 }
 
-void execute_one_line (int progc, char **cmds, int *stdfd, int *connection)
+void set_npipe (char *line, int pipefd[MAX_PIPE + 1][2], int *pndx, int *pipec, int *connection)
+{
+	int i, number;
+
+	/* shift all the pndx by one (count down timer) */
+	for (i = 0; i < MAX_PIPE - 1; ++i)
+		pndx[i] = pndx[i + 1];
+	pndx[MAX_PIPE] = 0;
+
+	/* set up the input from pipe */
+	if (pndx[0] != 0) {
+		close (pipefd[pndx[0]][1]);
+		close (STDIN_FILENO);
+		dup (pipefd[pndx[0]][0]);
+		close (pipefd[pndx[0]][0]);
+		--(*pipec);
+	}
+
+	/* find the numbered pipe in the current input line and add it in */
+	for (i = 0; line[i] != 0; ++i) {
+		if ((line[i] == '|' || line[i] == '!') && isnumber (line + i + 1)) {
+			/* the pipe number */
+			number = atoi (line + i + 1);
+
+			/* create a new pipe */
+			if (pndx[number] == 0) {
+				pndx[number] = *pipec;
+				if (pipe (pipefd[*pipec]) < 0) {
+					fputs ("server error: failed to create numbered pipes\n", stderr);
+					*connection = 0;
+				}
+				++(*pipec);
+			}
+
+			/* set up the output to pipe */
+			close (STDOUT_FILENO);
+			dup (pipefd[pndx[number]][1]);
+			if (line[i] == '!') {
+				close (STDERR_FILENO);
+				dup (pipefd[pndx[number]][1]);
+			}
+
+			line[i] = 0;
+			break;
+		}
+	}
+}
+
+void execute_one_line (int progc, char **cmds, int *connection)
 {
 	pid_t	childpid;
-	int	i, argc, pipefd[2];
+	int	i, argc, pipefd[2], stdfd[3];
 	char	*argv[MAX_CMD_SIZE / 2 + 1] = {0}, *in_file = NULL, *out_file = NULL;
+
+	/* save original fds */
+	save_stdfds (stdfd);
+
 	for (i = 0; i < progc; ++i) {
 		/* create a pipe */
 		if (pipe(pipefd) < 0) {
@@ -115,6 +179,9 @@ void execute_one_line (int progc, char **cmds, int *stdfd, int *connection)
 		/* free the allocated space of one command */
 		clear_argv (argc, argv, &in_file, &out_file);
 	}
+
+	/* restore original fds */
+	restore_stdfds (stdfd);
 }
 
 void setupenv (int argc, char **argv)
@@ -156,6 +223,20 @@ void save_stdfds (int *stdfd)
 {
 	stdfd[0] = dup (STDIN_FILENO);
 	stdfd[1] = dup (STDOUT_FILENO);
+	stdfd[2] = dup (STDERR_FILENO);
+}
+
+void restore_stdfds (int *stdfd)
+{
+	close (STDIN_FILENO);
+	dup (stdfd[0]);
+	close (stdfd[0]);
+	close (STDOUT_FILENO);
+	dup (stdfd[1]);
+	close (stdfd[1]);
+	close (STDERR_FILENO);
+	dup (stdfd[2]);
+	close (stdfd[2]);
 }
 
 void open_files (const char *in_file, const char *out_file)
@@ -200,18 +281,23 @@ int readline (char *line, int *connection)
 }
 
 
-int check_illegal (const char *line)
+int check_illegal (char *line)
 {
-	int i = 0;
-	while (line[i] != 0) {
+	int i;
+	for (i = 0; line[i] != 0; ++i) {
 		if (line[i] == '/') {
-			fputs ("input error: character '/' is not permitted\n", stderr);
+			fputs ("input error: character '/' is illegal\n", stderr);
 			return -1;
 		} else if ( line[i] == '|' && ! isdigit (line[i+1]) && ! isspace (line[i+1]) ) {
-			fprintf (stderr, "input error: \"%c%c\" cannot be recognized\n", line[i], line[i+1]);
+			fprintf (stderr, "input error: \"%c%c\" is illegal\n", line[i], line[i+1]);
 			return -1;
+		} else if (line[i] == '\r' || line[i] == '\n') {
+			line[i] = 0;
+			line[i + 1] = 0;
+			if (arespace (line))
+				return -1;
+			return 0;
 		}
-		++i;
 	}
 	return 0;
 }
@@ -305,4 +391,22 @@ void clear_cmds (int progc, char **cmds)
 		free (cmds[progc]);
 		cmds[progc] = NULL;
 	}
+}
+
+int isnumber (char *s)
+{
+	int i;
+	for (i = 0; s[i] != 0; ++i)
+		if (!isdigit(s[i]))
+			return 0;
+	return 1;
+}
+
+int arespace (char *s)
+{
+	int i;
+	for (i = 0; s[i] != 0; ++i)
+		if (!isspace(s[i]))
+			return 0;
+	return 1;
 }
