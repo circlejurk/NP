a65b6aba1d2ddc7648c7de607f88160a05093058
niorehkids <forendef2846@gmail.com>
2016-10-25 00:57:05 +0800

    add test/

---
+++ test/runclient.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+./client nplinux2.cs.nctu.edu.tw in1 > output/out1
+./client nplinux2.cs.nctu.edu.tw in2 > output/out2
+./client nplinux2.cs.nctu.edu.tw in3 > output/out3
+./client nplinux2.cs.nctu.edu.tw in4 > output/out4
+./client nplinux2.cs.nctu.edu.tw in5 > output/out5
+./client nplinux2.cs.nctu.edu.tw in6 > output/out6
