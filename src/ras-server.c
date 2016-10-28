77144194e868ba772e1471a3e7883fe62010d792
niorehkids <forendef2846@gmail.com>
2016-10-28 09:56:03 +0800

    add and use the function passiveTCP

--- src/ras-server.c
+++ src/ras-server.c
@@ -16,48 +16,28 @@
 
 int shell (void);
 void reaper (int sig);
+int passiveTCP (int port, int qlen);
 
 int main (void)
 {
-	int			sockfd, newsockfd;
+	int			msock, ssock;
 	socklen_t		clilen;
 	pid_t			childpid;
-	struct sockaddr_in	cli_addr, serv_addr;
+	struct sockaddr_in	cli_addr;
 
+	/* initialize the original directory */
 	chdir ("/u/cs/103/0310004/ras");
 
 	/* establish a signal handler for SIGCHLD */
 	signal (SIGCHLD, reaper);
 
-	/* open a TCP socket */
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		fputs ("server error: cannot open socket\n", stderr);
-		exit (1);
-	}
-
-	/* set up server socket addr */
-	bzero (&serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl (INADDR_ANY);
-	serv_addr.sin_port = htons (SERV_TCP_PORT);
-
-	/* bind to server address */
-	if (bind (sockfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-		fputs ("server error: cannot bind local address\n", stderr);
-		exit (1);
-	}
-
-	/* listen for requests */
-	if (listen (sockfd, 0) < 0) {
-		fputs ("server error: listen failed\n", stderr);
-		exit (1);
-	}
+	/* build a TCP connection */
+	msock = passiveTCP (SERV_TCP_PORT, 0);
 
 	while (1) {
 		/* accept connection request */
-		clilen = sizeof(cli_addr);
-		newsockfd = accept (sockfd, (struct sockaddr *)&cli_addr, &clilen);
-		if (newsockfd < 0) {
+		ssock = accept (msock, (struct sockaddr *)&cli_addr, &clilen);
+		if (ssock < 0) {
 			if (errno == EINTR)
 				continue;
 			fputs ("server error: accept failed\n", stderr);
@@ -72,14 +52,14 @@ int main (void)
 			close (STDIN_FILENO);
 			close (STDOUT_FILENO);
 			close (STDERR_FILENO);
-			dup (newsockfd);
-			dup (newsockfd);
-			dup (newsockfd);
-			close (sockfd);
-			close (newsockfd);
+			dup (ssock);
+			dup (ssock);
+			dup (ssock);
+			close (msock);
+			close (ssock);
 			exit (shell ());
 		}
-		close (newsockfd);
+		close (ssock);
 	}
 
 	return 0;
@@ -89,4 +69,36 @@ void reaper (int sig)
 {
 	while (waitpid (-1, NULL, WNOHANG) > 0);
 	signal (sig, reaper);
+}
+
+int passiveTCP (int port, int qlen)
+{
+	int			sockfd;
+	struct sockaddr_in	serv_addr;
+
+	/* open a TCP socket */
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		fputs ("server error: cannot open socket\n", stderr);
+		exit (1);
+	}
+
+	/* set up server socket addr */
+	bzero (&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+	serv_addr.sin_port = htons (port);
+
+	/* bind to server address */
+	if (bind (sockfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		fputs ("server error: cannot bind local address\n", stderr);
+		exit (1);
+	}
+
+	/* listen for requests */
+	if (listen (sockfd, qlen) < 0) {
+		fputs ("server error: listen failed\n", stderr);
+		exit (1);
+	}
+
+	return sockfd;
 }
