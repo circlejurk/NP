60a8fa70d2168f3d911c3dbbdf64cea28cff38c1
niorehkids <forendef2846@gmail.com>
2016-10-28 10:13:27 +0800

    add port number

--- test/runclient.sh
+++ test/runclient.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-./client nplinux2.cs.nctu.edu.tw in1 > output/out1
-./client nplinux2.cs.nctu.edu.tw in2 > output/out2
-./client nplinux2.cs.nctu.edu.tw in3 > output/out3
-./client nplinux2.cs.nctu.edu.tw in4 > output/out4
-./client nplinux2.cs.nctu.edu.tw in5 > output/out5
-./client nplinux2.cs.nctu.edu.tw in6 > output/out6
+./client nplinux2.cs.nctu.edu.tw 9527 in1 > output/out1
+./client nplinux2.cs.nctu.edu.tw 9527 in2 > output/out2
+./client nplinux2.cs.nctu.edu.tw 9527 in3 > output/out3
+./client nplinux2.cs.nctu.edu.tw 9527 in4 > output/out4
+./client nplinux2.cs.nctu.edu.tw 9527 in5 > output/out5
+./client nplinux2.cs.nctu.edu.tw 9527 in6 > output/out6
