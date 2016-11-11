5bad272a99c1be5fe7ea90c160c6af9462b1137a
niorehkids <forendef2846@gmail.com>
2016-11-12 02:54:57 +0800

    basic shell functionality

---
+++ 2/src/single/server.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#define __USE_MISC
+#define __USE_XOPEN2K
+#include <stdlib.h>
+#undef __USE_MISC
+#undef __USE_XOPEN2K
+#include <strings.h>
+#include <string.h>
+#include <stdint.h>
+#ifndef __USE_MISC
+#define __USE_MISC
+#endif
+#include <unistd.h>
+#undef __USE_MISC
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "single.h"
+
+int passiveTCP (int port, int qlen);
+void add_user (int sock, struct sockaddr_in *cli_addr, User *users);
+void rm_user (User *users);
+void execute (int fd, User *users);
+
+int main (void)
+{
+	int			msock, ssock, fd, nfds = getdtablesize ();
+	fd_set			rfds, afds;
+	socklen_t		clilen = sizeof (struct sockaddr_in);
+	struct sockaddr_in	cli_addr = {0};
+	User			users[MAX_USERS + 1] = {0};
+
+	/* initialize the original directory */
+	/*chdir ("/u/cs/103/0310004/rwg");*/
+
+	/* initialize the environment variables */
+	clearenv ();
+	putenv ("PATH=bin:.");
+
+	/* close the original fds */
+	close (STDIN_FILENO);
+	close (STDOUT_FILENO);
+	close (STDERR_FILENO);
+
+	/* build a TCP connection */
+	if ((msock = passiveTCP (SERV_TCP_PORT, 0)) < 0)
+		return -1;
+
+	/* initialize the active file descriptor set */
+	FD_ZERO (&afds);
+	FD_SET (msock, &afds);
+
+	while (1) {
+		/* copy the active fds into read fds */
+		memcpy (&rfds, &afds, sizeof(rfds));
+		/* select from the rfds */
+		if (select (nfds, &rfds, NULL, NULL, NULL) < 0)
+			return -1;
+		/* check for new connection requests */
+		if (FD_ISSET (msock, &rfds)) {
+			ssock = accept (msock, (struct sockaddr *)&cli_addr, &clilen);
+			if (ssock < 0)
+				return -1;
+			FD_SET (ssock, &afds);	/* add an active socket */
+			add_user (ssock, &cli_addr, users);	/* add a user */
+		}
+		/* handle one line command if needed */
+		for (fd = 0; fd < nfds; ++fd) {
+			if (fd != msock && FD_ISSET (fd, &rfds)) {
+				execute (fd, users);
+				if (users[fd].connection < 0) {
+					return -1;
+				} else if (users[fd].connection == 0) {
+					FD_CLR (fd, &afds);
+					rm_user (users + fd);
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+void execute (int fd, User *users)
+{
+	int	stdfd[3];
+	save_fds (stdfd);
+	dup2 (fd, STDIN_FILENO);
+	dup2 (fd, STDOUT_FILENO);
+	dup2 (fd, STDERR_FILENO);
+	shell (users + fd);
+	restore_fds (stdfd);
+	if (fd == 1) {
+		dup2 (2, 0);
+		close (2);
+	}
+	write (fd, prompt, strlen(prompt));	/* show the prompt */
+}
+
+void rm_user (User *user)
+{
+	close (user->sock);
+	user->sock = 0;
+	user->name[0] = 0;
+	user->ip[0] = 0;
+	user->port = 0;
+	user->connection = 0;
+	clear_nps (user->np);	/* free the allocated space of numbered pipes */
+	user->np->fd = NULL;
+	/* broadcast that you're fucking out */
+}
+
+void add_user (int sock, struct sockaddr_in *cli_addr, User *users)
+{
+	users[sock].sock = sock;
+	strcpy (users[sock].name, "no name");
+	strcpy (users[sock].ip, inet_ntoa (cli_addr->sin_addr));
+	users[sock].port = cli_addr->sin_port;
+	users[sock].connection = 1;
+
+	write (sock, motd, strlen(motd));	/* print the welcome message */
+	write (sock, prompt, strlen(prompt));	/* show the prompt */
+	/* broadcast that you're fucking in */
+}
+
+int passiveTCP (int port, int qlen)
+{
+	int			sockfd;
+	struct sockaddr_in	serv_addr;
+
+	/* open a TCP socket */
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		fputs ("server error: cannot open socket\n", stderr);
+		return -1;
+	}
+
+	/* set up server socket addr */
+	bzero (&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+	serv_addr.sin_port = htons (port);
+
+	/* bind to server address */
+	if (bind (sockfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		fputs ("server error: cannot bind local address\n", stderr);
+		return -1;
+	}
+
+	/* listen for requests */
+	if (listen (sockfd, qlen) < 0) {
+		fputs ("server error: listen failed\n", stderr);
+		return -1;
+	}
+
+	return sockfd;
+}
---
+++ 2/src/single/shell.c
@@ -0,0 +1,396 @@
+#include <stdio.h>
+#define __USE_MISC
+#define __USE_XOPEN2K
+#include <stdlib.h>
+#undef __USE_MISC
+#undef __USE_XOPEN2K
+#include <ctype.h>
+#include <string.h>
+#define __USE_GNU
+#include <unistd.h>
+#undef __USE_GNU
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "single.h"
+
+const char motd[] =	"****************************************\n"
+			"** Welcome to the information server. **\n"
+			"****************************************\n\n";
+const char prompt[] = "% ";
+
+
+int shell (User *user)
+{
+	int	progc, stdfd[3], readc;
+	char	line[MAX_LINE_SIZE + 1], *cmds[(MAX_LINE_SIZE - 1) / 2];
+
+	if (user->connection > 0) {
+		/* read one line from client input */
+		if ((readc = readline (line, &(user->connection))) < 0) {
+			/* close numbered pipe for illegal input */
+			close_np (user->np);
+		} else if (readc > 0) {
+			/* save original fds */
+			save_fds (stdfd);
+			/* add new numbered pipe and set up to output */
+			set_np_out (line, user->np, &(user->connection));
+			/* set up the input from numbered pipe */
+			set_np_in (user->np);
+			/* parse the total input line into commands seperated by pipes */
+			progc = line_to_cmds (line, cmds);
+			/* execute one line command */
+			execute_one_line (progc, cmds, &(user->connection));
+			/* restore original fds */
+			restore_fds (stdfd);
+			/* free the allocated space of commands */
+			clear_cmds (progc, cmds);
+			/* shift all the numbered pipes by one (count down timer) */
+			np_countdown (user->np);
+		}
+	}
+
+	return user->connection;
+}
+
+int readline (char *line, int *connection)
+{
+	int	i;
+	char	*p = NULL;
+	p = fgets (line, MAX_LINE_SIZE + 1, stdin);
+	if (ferror (stdin)) {
+		fputs ("read error: fgets failed\n", stderr);
+		return -1;
+	} else if (feof (stdin))	/* EOF */
+		*connection = 0;
+	if (p == NULL)		/* no data was read */
+		return 0;
+	/* check for illegal input and remove crlf */
+	for (i = 0; line[i] != 0; ++i) {
+		if (line[i] == '/') {
+			fputs ("input error: character '/' is illegal\n", stderr);
+			return -1;
+		} else if (line[i] == '\r' || line[i] == '\n') {
+			line[i] = 0;
+			line[i + 1] = 0;
+			if (arespace (line))
+				return 0;
+			break;
+		}
+	}
+	return strlen (line);
+}
+
+void clear_nps (Npipe np[MAX_PIPE])
+{
+	int	 i;
+	for (i = 0; i < MAX_PIPE; ++i) {
+		if (np[i].fd) {
+			free (np[i].fd);
+			np[i].fd = NULL;
+		}
+	}
+}
+
+void close_np (Npipe np[MAX_PIPE])
+{
+	if (np[0].fd) {
+		close (np[0].fd[1]);
+		close (np[0].fd[0]);
+		free (np[0].fd);
+	}
+}
+
+void np_countdown (Npipe np[MAX_PIPE])
+{
+	int	i;
+	for (i = 0; i < MAX_PIPE - 1; ++i)
+		np[i] = np[i + 1];
+	np[MAX_PIPE - 1].fd = NULL;
+}
+
+void set_np_in (Npipe np[MAX_PIPE])
+{
+	if (np[0].fd) {
+		close (np[0].fd[1]);
+		close (STDIN_FILENO);
+		dup (np[0].fd[0]);
+		close (np[0].fd[0]);
+		free (np[0].fd);
+	}
+}
+
+void set_np_out (char *line, Npipe np[MAX_PIPE], int *connection)
+{
+	int	i, number;
+
+	/* find the numbered pipe in the current input line and add it in */
+	for (i = 0; line[i] != 0; ++i) {
+		if ((line[i] == '|' || line[i] == '!') && isnumber (line + i + 1)) {
+			/* the pipe number */
+			number = atoi (line + i + 1);
+
+			/* create a new pipe if it has not been created */
+			if (np[number].fd == NULL) {
+				np[number].fd = (int *) malloc (2 * sizeof(int));
+				if (pipe (np[number].fd) < 0) {
+					fputs ("server error: failed to create numbered pipes\n", stderr);
+					*connection = 0;
+				}
+			}
+
+			/* set up the output to pipe */
+			close (STDOUT_FILENO);
+			dup (np[number].fd[1]);
+			if (line[i] == '!') {
+				close (STDERR_FILENO);
+				dup (np[number].fd[1]);
+			}
+
+			line[i] = 0;
+			break;
+		}
+	}
+}
+
+void execute_one_line (int progc, char **cmds, int *connection)
+{
+	pid_t	childpid;
+	int	i, argc, pipefd[2], stdfd[3], stat;
+	char	*argv[MAX_CMD_SIZE / 2 + 1] = {0}, *in_file = NULL, *out_file = NULL;
+
+	/* save original fds */
+	save_fds (stdfd);
+
+	for (i = 0; i < progc; ++i) {
+		/* parse the input command into argv */
+		argc = cmd_to_argv (cmds[i], argv, &in_file, &out_file);
+
+		/* execute the command accordingly */
+		if (*argv != NULL && strcmp (*argv, "exit") == 0) {
+			*connection = 0;
+		} else if (*argv != NULL && strcmp (*argv, "printenv") == 0) {
+			printenv (argc, argv);
+		} else if (*argv != NULL && strcmp (*argv, "setenv") == 0) {
+			setupenv (argc, argv);
+		} else {
+			/* create a pipe */
+			if (pipe(pipefd) < 0) {
+				fputs ("server error: failed to create pipes\n", stderr);
+				*connection = 0;
+				return;
+			}
+
+			if ((childpid = fork()) < 0) {
+				close (pipefd[0]); close (pipefd[1]);
+				fputs ("server error: fork failed\n", stderr);
+			} else if (childpid == 0) {
+				set_pipes_out (pipefd, stdfd, i, progc);
+				open_files (in_file, out_file);
+				execvpe (*argv, argv, environ);
+				fprintf (stderr, "Unknown command: [%s].\n", *argv);
+				*connection = -1;
+				i = progc;
+			} else {
+				set_pipes_in (pipefd, stdfd, i, progc);
+				wait (&stat);
+				if (stat != 0)
+					i = progc;
+			}
+		}
+
+		/* free the allocated space of one command */
+		clear_argv (argc, argv, &in_file, &out_file);
+	}
+
+	/* restore original fds */
+	restore_fds (stdfd);
+}
+
+void setupenv (int argc, char **argv)
+{
+	if (argc == 3) {
+		if (setenv (argv[1], argv[2], 1) < 0)
+			fputs ("server error: setenv() failed\n", stderr);
+	} else
+		fputs ("Usage: setenv <name> <value>\n", stderr);
+}
+
+void set_pipes_out (int *pipefd, int *stdfd, int index, int progc)
+{
+	if (index != progc - 1) {
+		close (STDOUT_FILENO);
+		dup (pipefd[1]);
+	} else {
+		close (STDOUT_FILENO);
+		dup (stdfd[1]);
+	}
+	close (pipefd[0]);
+	close (pipefd[1]);
+}
+
+void set_pipes_in (int *pipefd, int *stdfd, int index, int progc)
+{
+	if (index != progc - 1) {
+		close (STDIN_FILENO);
+		dup (pipefd[0]);
+	} else {
+		close (STDIN_FILENO);
+		dup (stdfd[0]);
+	}
+	close (pipefd[0]);
+	close (pipefd[1]);
+}
+
+void save_fds (int *stdfd)
+{
+	stdfd[0] = dup (STDIN_FILENO);
+	stdfd[1] = dup (STDOUT_FILENO);
+	stdfd[2] = dup (STDERR_FILENO);
+}
+
+void restore_fds (int *stdfd)
+{
+	dup2 (stdfd[0], STDIN_FILENO);
+	close (stdfd[0]);
+	dup2 (stdfd[1], STDOUT_FILENO);
+	close (stdfd[1]);
+	dup2 (stdfd[2], STDERR_FILENO);
+	close (stdfd[2]);
+}
+
+void open_files (const char *in_file, const char *out_file)
+{
+	int	infd, outfd;
+	if (in_file) {
+		infd = open (in_file, O_RDONLY, 0);
+		if (infd < 0) {
+			fprintf (stderr, "server error: cannot open file %s\n", in_file);
+			exit (1);
+		}
+		close (STDIN_FILENO);
+		dup (infd);
+		close (infd);
+	}
+	if (out_file) {
+		outfd = open (out_file, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+		if (outfd < 0) {
+			fprintf (stderr, "server error: cannot open file %s\n", out_file);
+			exit (1);
+		}
+		close (STDOUT_FILENO);
+		dup (outfd);
+		close (outfd);
+	}
+}
+
+void printenv (int argc, char **argv)
+{
+	int	i;
+	if (argc == 1) {
+		for (i = 0; environ[i] != NULL; ++i) {
+			char *out;
+			out = (char *) malloc (strlen(environ[i]) + 2);
+			strcpy (out, environ[i]);
+			strcpy (out + strlen(environ[i]), "\n");
+			write (STDOUT_FILENO, out, strlen(out));
+			free (out);
+		}
+	} else {
+		for (i = 1; i < argc; ++i) {
+			char *value = getenv (argv[i]), *out;
+			if (value == NULL) {
+				fprintf (stderr, "\"%s\" does not exist\n", argv[i]);
+			} else {
+				out = (char *) malloc (strlen(argv[i]) + 1 + strlen(value) + 2);
+				strcpy (out, argv[i]);
+				strcpy (out + strlen(argv[i]), "=");
+				strcpy (out + strlen(argv[i]) + 1, value);
+				strcpy (out + strlen(argv[i]) + 1 + strlen(value), "\n");
+				write (STDOUT_FILENO, out, strlen(out));
+				free (out);
+			}
+		}
+	}
+}
+
+int cmd_to_argv (char *cmd, char **argv, char **in_file, char **out_file)
+{
+	int	argc = 0;
+	char	*arg;
+
+	arg = strtok (cmd, " \r\n");
+	while (arg != NULL) {
+		if (strcmp (arg, ">") == 0) {
+			arg = strtok (NULL, " \r\n");
+			*out_file = (char *) malloc (strlen(arg) + 1);
+			strcpy (*out_file, arg);
+		} else if (strcmp (arg, "<") == 0) {
+			arg = strtok (NULL, " \r\n");
+			*in_file = (char *) malloc (strlen(arg) + 1);
+			strcpy (*in_file, arg);
+		} else {
+			argv[argc] = (char *) malloc (strlen(arg) + 1);
+			strcpy (argv[argc], arg);
+			++argc;
+		}
+		arg = strtok (NULL, " \r\n");
+	}
+
+	return argc;
+}
+
+void clear_argv (int argc, char **argv, char **in_file, char **out_file)
+{
+	while (--argc >= 0) {
+		free (argv[argc]);
+		argv[argc] = NULL;
+	}
+	free (*in_file);
+	*in_file = NULL;
+	free (*out_file);
+	*out_file = NULL;
+}
+
+int line_to_cmds (char *line, char **cmds)
+{
+	int	progc = 0;
+	char	*cmd;
+
+	cmd = strtok (line, "|\r\n");
+	while (cmd != NULL ) {
+		cmds[progc] = (char *) malloc (strlen(cmd) + 1);
+		strcpy (cmds[progc], cmd);
+		++progc;
+		cmd = strtok (NULL, "|\r\n");
+	}
+	return progc;
+}
+
+void clear_cmds (int progc, char **cmds)
+{
+	while (--progc >= 0) {
+		free (cmds[progc]);
+		cmds[progc] = NULL;
+	}
+}
+
+int isnumber (char *s)
+{
+	int	i;
+	for (i = 0; s[i] != 0; ++i)
+		if (!isdigit(s[i]))
+			return 0;
+	return 1;
+}
+
+int arespace (char *s)
+{
+	int	i;
+	for (i = 0; s[i] != 0; ++i)
+		if (!isspace(s[i]))
+			return 0;
+	return 1;	/* empty strings are treated as spaces */
+}
