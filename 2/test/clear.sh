2715bb9b38eea3784a5d4416d2a9ee99de8a7ba9
niorehkids <forendef2846@gmail.com>
2016-12-02 16:09:38 +0800

    add new tests; add a clear script for cleaning outputs

---
+++ 2/test/clear.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -f out*
+
+cd /u/cs/103/0310004/rwg
+rm -f baozi *.txt
+cd -
--- 2/test/runclient.sh
+++ 2/test/runclient.sh
@@ -4,6 +4,9 @@
 ./delayclient localhost 9527 in2 > out2
 ./delayclient localhost 9527 in3 > out3
 ./delayclient localhost 9527 in4 > out4
+./delayclient localhost 9527 in5 > out5
+./delayclient localhost 9527 in6 > out6
+./delayclient localhost 9527 in7 > out7
 
 cd /u/cs/103/0310004/rwg
 rm -f baozi *.txt
