da747978184c97b2fcd9afc3784ab5f3e35ca6d3
niorehkids <forendef2846@gmail.com>
2016-12-04 18:38:26 +0800

    update runcgi.sh: change host to nplinux2

--- 3/public_html/runcgi.sh
+++ 3/public_html/runcgi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#REQUEST_METHOD="GET" CONTENT_LENGTH="0" QUERY_STRING="h1=localhost&p1=9527&f1=t1.txt&h2=&p2=&f2=&h3=&p3=&f3=&h4=&p4=&f4=&h5=&p5=&f5=" gdb -q ./hw3.cgi
-REQUEST_METHOD="GET" CONTENT_LENGTH="0" QUERY_STRING="h1=localhost&p1=9527&f1=t1.txt&h2=&p2=&f2=&h3=&p3=&f3=&h4=&p4=&f4=&h5=&p5=&f5=" ./hw3.cgi
+#REQUEST_METHOD="GET" CONTENT_LENGTH="0" QUERY_STRING="h1=nplinux2.cs.nctu.edu.tw&p1=9527&f1=t1.txt&h2=&p2=&f2=&h3=&p3=&f3=&h4=&p4=&f4=&h5=&p5=&f5=" gdb -q ./hw3.cgi
+REQUEST_METHOD="GET" CONTENT_LENGTH="0" QUERY_STRING="h1=nplinux2.cs.nctu.edu.tw&p1=9527&f1=t1.txt&h2=&p2=&f2=&h3=&p3=&f3=&h4=&p4=&f4=&h5=&p5=&f5=" ./hw3.cgi
