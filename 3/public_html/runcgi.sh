1bd2d4d8405a3c8ab6cefbd7d0742d035aae87df
niorehkids <forendef2846@gmail.com>
2016-12-05 02:47:25 +0800

    update runcgi.sh

--- 3/public_html/runcgi.sh
+++ 3/public_html/runcgi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#REQUEST_METHOD="GET" CONTENT_LENGTH="0" QUERY_STRING="h1=nplinux2.cs.nctu.edu.tw&p1=9527&f1=t1.txt&h2=&p2=&f2=&h3=&p3=&f3=&h4=&p4=&f4=&h5=&p5=&f5=" gdb -q ./hw3.cgi
-REQUEST_METHOD="GET" CONTENT_LENGTH="0" QUERY_STRING="h1=nplinux2.cs.nctu.edu.tw&p1=9527&f1=t1.txt&h2=&p2=&f2=&h3=&p3=&f3=&h4=&p4=&f4=&h5=&p5=&f5=" ./hw3.cgi
+#REQUEST_METHOD="GET" CONTENT_LENGTH="0" QUERY_STRING="h1=nplinux2.cs.nctu.edu.tw&p1=9527&f1=t1.txt&h2=nplinux2.cs.nctu.edu.tw&p2=9527&f2=t2.txt&h3=nplinux2.cs.nctu.edu.tw&p3=9527&f3=t3.txt&h4=nplinux2.cs.nctu.edu.tw&p4=9527&f4=t4.txt&h5=nplinux2.cs.nctu.edu.tw&p5=9527&f5=t5.txt" gdb -q ./hw3.cgi
+REQUEST_METHOD="GET" CONTENT_LENGTH="0" QUERY_STRING="h1=nplinux2.cs.nctu.edu.tw&p1=9527&f1=t1.txt&h2=nplinux2.cs.nctu.edu.tw&p2=9527&f2=t2.txt&h3=nplinux2.cs.nctu.edu.tw&p3=9527&f3=t3.txt&h4=nplinux2.cs.nctu.edu.tw&p4=9527&f4=t4.txt&h5=nplinux2.cs.nctu.edu.tw&p5=9527&f5=t5.txt" ./hw3.cgi
